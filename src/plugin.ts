32dec0d1c7db7abbd798b8fc76980b84f14c9969
krypek <krypek@tuta.io>
2025-06-26 12:55:27 +0200

    Fix typo

--- src/plugin.ts
+++ src/plugin.ts
@@ -4,7 +4,7 @@ import ccmod from '../ccmod.json'
 import { registerOpts } from './options'
 import { inputFieldBackwardsCompatibility } from './ui/input-field-combatibility'
 import { injectQuickMenuExtension } from './ui/quick-menu/quick-menu-extension'
-import { pauseScreenApiPreload, pauseScreenApiPrestart } from './ui/title-screen/title-screen-api'
+import { pauseScreenApiPreload, pauseScreenApiPrestart } from './ui/pause-screen/pause-screen-api'
 import { setupQuickMenuPreload } from './ui/quick-menu/quick-menu-preload'
 
 export default class CCUILib implements PluginClass {
